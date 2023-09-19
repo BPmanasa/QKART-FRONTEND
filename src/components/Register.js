056d42058c4b5f7c592c13847b7d42371b264384
BPmanasa <manasabpmanasabp77@gmail.com>
2023-09-19 14:48:47 +0530

    “ME_QKART_FRONTEND_V2_MODULE_REGISTER”  (Crio.Do)

--- src/components/Register.js
+++ src/components/Register.js
@@ -11,53 +11,87 @@ import "./Register.css";
 const Register = () => {
   const { enqueueSnackbar } = useSnackbar();
 
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    confirmPassword: ""
+  });
 
-  // TODO: CRIO_TASK_MODULE_REGISTER - Implement the register function
-  /**
-   * Definition for register handler
-   * - Function to be called when the user clicks on the register button or submits the register form
-   *
-   * @param {{ username: string, password: string, confirmPassword: string }} formData
-   *  Object with values of username, password and confirm password user entered to register
-   *
-   * API endpoint - "POST /auth/register"
-   *
-   * Example for successful response from backend for the API call:
-   * HTTP 201
-   * {
-   *      "success": true,
-   * }
-   *
-   * Example for failed response from backend for the API call:
-   * HTTP 400
-   * {
-   *      "success": false,
-   *      "message": "Username is already taken"
-   * }
-   */
-  const register = async (formData) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
-  // TODO: CRIO_TASK_MODULE_REGISTER - Implement user input validation logic
-  /**
-   * Validate the input values so that any bad or illegal values are not passed to the backend.
-   *
-   * @param {{ username: string, password: string, confirmPassword: string }} data
-   *  Object with values of username, password and confirm password user entered to register
-   *
-   * @returns {boolean}
-   *    Whether validation has passed or not
-   *
-   * Return false if any validation condition fails, otherwise return true.
-   * (NOTE: The error messages to be shown for each of these cases, are given with them)
-   * -    Check that username field is not an empty value - "Username is a required field"
-   * -    Check that username field is not less than 6 characters in length - "Username must be at least 6 characters"
-   * -    Check that password field is not an empty value - "Password is a required field"
-   * -    Check that password field is not less than 6 characters in length - "Password must be at least 6 characters"
-   * -    Check that confirmPassword field has the same value as password field - Passwords do not match
-   */
+  const handleRegister = async () => {
+    if(!validateInput(formData)) return;
+
+      
+      try {
+        setLoading(true);
+        await axios.post(`${config.endpoint}/auth/register`,{
+          username: formData.username,
+          password: formData.password,
+        });
+        setLoading(false);
+        // setFormData({
+        //   username:"",
+        //   password:"",
+        //   confirmPassword:""
+        // });
+        enqueueSnackbar("Registered Successfully", {variant: "success"});
+  
+        // history.push("/login");
+  
+      }
+      catch(e){
+        setLoading(false);
+        if(e.response && e.response.status === 400){
+          enqueueSnackbar(e.response.data.message, {variant: "error"});
+        }else{
+          enqueueSnackbar("Something went. Check that the backend is running, reachable and return valid JSON.",
+          {variant: "error"}
+          );
+        }
+      }
+  
+      };
+      
   const validateInput = (data) => {
+    const { username, password, confirmPassword } = data;
+
+    if (!username) {
+      enqueueSnackbar("Username is a required field", { variant: "error" });
+      return false;
+    }
+
+    if (username.length < 6) {
+      enqueueSnackbar("Username must be at least 6 characters", { variant: "error" });
+      return false;
+    }
+
+    if (!password) {
+      enqueueSnackbar("Password is a required field", { variant: "error" });
+      return false;
+    }
+
+    if (password.length < 6) {
+      enqueueSnackbar("Password must be at least 6 characters", { variant: "error" });
+      return false;
+    }
+
+    if (password !== confirmPassword) {
+      enqueueSnackbar("Passwords do not match", { variant: "error" });
+      return false;
+    }
+
+    return true;
   };
+
 
   return (
     <Box
@@ -78,6 +112,8 @@ const Register = () => {
             name="username"
             placeholder="Enter Username"
             fullWidth
+            value={formData.username}
+            onChange={handleInputChange}
           />
           <TextField
             id="password"
@@ -85,9 +121,11 @@ const Register = () => {
             label="Password"
             name="password"
             type="password"
-            helperText="Password must be atleast 6 characters length"
+            helperText="Password must be at least 6 characters length"
             fullWidth
             placeholder="Enter a password with minimum 6 characters"
+            value={formData.password}
+            onChange={handleInputChange}
           />
           <TextField
             id="confirmPassword"
@@ -96,15 +134,22 @@ const Register = () => {
             name="confirmPassword"
             type="password"
             fullWidth
+            value={formData.confirmPassword}
+            onChange={handleInputChange}
           />
-           <Button className="button" variant="contained">
-            Register Now
-           </Button>
+          <Button
+            className="button"
+            variant="contained"
+            onClick={handleRegister}
+            disabled={loading}
+          >
+            {loading ? <CircularProgress size={24} /> : "Register Now"}
+          </Button>
           <p className="secondary-action">
             Already have an account?{" "}
-             <a className="link" href="#">
+            <a className="link" href="#">
               Login here
-             </a>
+            </a>
           </p>
         </Stack>
       </Box>
